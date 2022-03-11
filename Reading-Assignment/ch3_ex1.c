54a04215244038eedce2ad1490ffa6302f413a34
rainiken27 <99723586+rainiken27@users.noreply.github.com>
2022-03-11 20:23:51 +0800

    Lecture 1 Reading Assignment

---
+++ Reading-Assignment/ch3_ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+   int i, j; 
+   float x, y;
+
+   //arbitrary integer values
+   i = 10;
+   j = 20;
+
+   //arbitrary float values
+   x = 43.2892f;
+   y = 5527.0f;
+
+   //function prints variable names and their corresponding values
+   printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+}
