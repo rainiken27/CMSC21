93865dcbc31c9117aa5ad18b781e8511ac434779
rainiken27 <99723586+rainiken27@users.noreply.github.com>
2022-03-11 20:33:50 +0800

    Add files via upload

---
+++ Assignments/assignment_lec1_c1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+   //text includes good programming practices and tips in the C language
+   printf("In C, lowercase letters are significant.\n");
+   printf("main is where program execution begins.\n");
+   printf("Opening and closing braces enclose program statements in a routine.\n");
+   printf("All program statements must be terminated by a semicolon.\n");
+
+}
